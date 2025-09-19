9f39af950ce89801e98d443f8f47c8f40abee74c
Ioannis Mallios <i.mallios@systemic.local>
2025-09-19 10:27:49 +0300

    First commit

---
+++ FetchDataFromApiTraining/src/Enums.jsx
@@ -0,0 +1,14 @@
+
+
+export const ProfilePics = {
+
+    MALE: 'https://cdn-icons-png.flaticon.com/128/4140/4140037.png',
+        FEMALE: 'https://cdn-icons-png.flaticon.com/128/4140/4140047.png'
+}
+
+export const Grade = {
+    FRESHMAN: "Freshman",
+    SOPHOMORE: "Sophomore",
+    JUNIOR: "Junior",
+    SENIOR: "Senior",
+};
---
+++ FetchDataFromApiTraining/src/Models/Student.jsx
@@ -0,0 +1,15 @@
+
+const Student = {
+    id: "",             
+    pictureUrl: "",     
+    firstName: "",      
+    lastName: "",       
+    age: 0,             
+    email: "",          
+    phone: "",          
+    grade: "",          
+    major: "",           
+    address: "",        
+    enrollmentDate: "", 
+    isActive: true,     
+};
