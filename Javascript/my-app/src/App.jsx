d72ca7d31dc2294a37133a6bb9183f88ee791630
Ioannis Mallios <i.mallios@systemic.local>
2025-10-07 12:43:12 +0300

    tic tac toe

--- Javascript/my-app/src/App.jsx
+++ Javascript/my-app/src/App.jsx
@@ -2,26 +2,36 @@ import { useState } from 'react'
 import './App.css'
 
 
-const Greeting = ({name,lname}) => (
-    <h1>Hello {name} {lname}</h1>
+const Greeting = ({name}) => (
+    <h1>Hello {name}</h1>
 )
 
 const MyButton = ({ trigger, label }) => (
 
-    <button onClick={trigger} style={{ width: 200,height: 200}}>{label}</button>
+    <button onClick={trigger} style={{ width: 100,height: 100}}>{label}</button>
 )
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(""))
+    const [board, setBoard] = useState(Array(9).fill(""))
 
-  return (
-      <div>
-          <Greeting name="John" lname={board} />
-          <MyButton trigger={() => setBoard(board + 1)} />
-          {board.map((label,i) => (
-              <MyButton label={label} trigger={()=>setBoard(board.map((el,p)=> p===i ? "x" : el))} />
-          ))}
-    </div>
+    const [playerXfirst,setPlayer] = useState(true)
+
+    return (
+      <>
+        <div>
+                <Greeting name="John" />
+                <div style={{display: 'grid',gridTemplateColumns: 'repeat(3, 100px)',gap: '10px',justifyContent: 'center',marginTop: '20px'}}>
+                 
+                    {board.map((label, i) =>
+                        playerXfirst ? (<MyButton key={i} label={label} trigger={() =>setBoard(board.map((el, p) => (p === i ? 'x' : el)))}/>
+                        ) : ( <MyButton key={i} label={label} trigger={() => setBoard(board.map((el, p) => (p === i ? 'o' : el)))
+                                }
+                            />
+                        )
+                    )}
+                </div>
+        </div>
+    </>
   )
 }
 
