eece1d02bf16e7f5e926d6bdece232589d2b084b
Ioannis Mallios <i.mallios@systemic.local>
2025-11-06 12:05:31 +0200

    delete some small projects

--- Javascript/test/src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import destinationReducer from "./destinationSlice";
-
-export const store = configureStore({
-    reducer: {
-        destinations: destinationReducer,
-    },
-});
