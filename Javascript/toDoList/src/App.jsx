a11d68a5b9f94fb38defbd7a949ed5fd5817ffb5
Ioannis Mallios <i.mallios@systemic.local>
2026-02-09 16:49:48 +0200

    complete simple to do list

--- Javascript/toDoList/src/App.jsx
+++ Javascript/toDoList/src/App.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addTask, deleteTask } from "./reducer/toDoSlice";
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
@@ -11,11 +12,18 @@ function App() {
     <div>
       <h2>Tasks: {tasks.length}</h2>
 
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-
-      <button onClick={() => dispatch(addTask(text))}>Add</button>
-
-      <button onClick={() => dispatch(deleteTask(0))}>Delete first</button>
+      <div className="container">
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={() => dispatch(addTask(text))}>Add</button>
+        <button onClick={() => dispatch(deleteTask(0))}>Remove</button>
+      </div>
+      <div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
