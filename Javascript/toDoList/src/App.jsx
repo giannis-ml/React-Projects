86087c8027887808a2bcd08e1e2a6eae1116de1f
Ioannis Mallios <i.mallios@systemic.local>
2026-02-10 12:23:26 +0200

    create todo list app

--- Javascript/toDoList/src/App.jsx
+++ Javascript/toDoList/src/App.jsx
@@ -1,31 +1,35 @@
-import { useDispatch, useSelector } from "react-redux";
-import { addTask, deleteTask } from "./reducer/toDoSlice";
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [text, setText] = useState("");
-  const tasks = useSelector((state) => state.todo.tasks);
-  const dispatch = useDispatch();
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h2>Tasks: {tasks.length}</h2>
-
-      <div className="container">
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-        <button onClick={() => dispatch(addTask(text))}>Add</button>
-        <button onClick={() => dispatch(deleteTask(0))}>Remove</button>
-      </div>
+    <>
       <div>
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}
-        </ul>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- Javascript/toDoList/src/main.jsx
+++ Javascript/toDoList/src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import { store } from "./store/store.js";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <Provider store={store}>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </Provider>,
-);
+  </StrictMode>,
+)
