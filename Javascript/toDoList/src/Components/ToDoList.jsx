d956e869f2c4ba8062cf4cb9d693048c257ccbdd
Ioannis Mallios <i.mallios@systemic.local>
2026-02-11 11:05:17 +0200

    Add/Delete completed

--- Javascript/toDoList/src/Components/ToDoList.jsx
+++ Javascript/toDoList/src/Components/ToDoList.jsx
@@ -10,11 +10,26 @@ function ToDoList() {
 
   const handleAdd = () => {
     if (input.trim() === "") return;
-    setTasks([...tasks, input]);
+    const newTask = {
+      id: Date.now(),
+      text: input,
+      checked: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setInput("");
   };
 
   const handleDelete = () => {
-    alert("GoodBye");
+    const filteredTasks = tasks.filter((task) => !task.checked);
+    setTasks(filteredTasks);
+  };
+
+  const handleCheck = (id) => {
+    const updateTasks = tasks.map((task) =>
+      task.id === id ? { ...task, checked: !task.checked } : task,
+    );
+    setTasks(updateTasks);
   };
 
   return (
@@ -25,8 +40,8 @@ function ToDoList() {
         <input
           type="text"
           className="form-control"
-          id="inputPassword2"
           placeholder="Enter your task..."
+          value={input}
           onChange={(e) => {
             setInput(e.target.value);
           }}
@@ -34,23 +49,22 @@ function ToDoList() {
       </div>
 
       <div className="form-group mx-sm-3 mb-2">
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
               <th scope="col">
                 <button
                   type="button"
-                  class="btn btn-outline-primary"
+                  className="btn btn-outline-primary"
                   onClick={handleAdd}
                 >
-                  Primary
+                  Add
                 </button>
               </th>
               <th scope="col">
-                {" "}
                 <button
                   type="button"
-                  class="btn btn-outline-danger"
+                  className="btn btn-outline-danger"
                   on
                   onClick={handleDelete}
                 >
@@ -61,8 +75,20 @@ function ToDoList() {
           </thead>
         </table>
       </div>
-
-      {tasks}
+      <ul className="list-group mx-sm-3">
+        {tasks.map((task) => (
+          <li key={task.id} className="list-group-item">
+            <input
+              className="form-check-input me-1"
+              type="checkbox"
+              checked={task.checked}
+              aria-label="..."
+              onChange={() => handleCheck(task.id)}
+            />
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
