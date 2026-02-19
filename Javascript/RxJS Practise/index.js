1c1f4bc7275e88986eb26b5d8ed9d5354a4b0a1b
Giannis_Mallios <giannismallios2001@gmail.com>
2026-02-19 10:50:55 +0200

    Add ReadMe, add Comments

--- Javascript/RxJS Practise/index.js
+++ Javascript/RxJS Practise/index.js
@@ -9,41 +9,43 @@ class Observable {
     this.subscription({ next, error, complete });
   }
 
-  //   pipe(...operations) {
-  //     return operations.reduce((stream, operation) => operation(stream), this);
-  //   }
+  //pipe is used for modifying data
+  pipe(...operations) {
+    return operations.reduce((stream, operation) => operation(stream), this);
+  }
 }
 
-// const map = (func) => (stream$) =>
-//   new Observable((observer) => {
-//     stream$.subscribe(
-//       (x) => {
-//         observer.next(func(x));
-//       },
-//       (error) => observer.error(error),
-//       () => observer.complete(),
-//     );
-//   });
+const map = (func) => (stream$) =>
+  new Observable((observer) => {
+    stream$.subscribe(
+      (x) => {
+        observer.next(func(x));
+      },
+      (error) => observer.error(error),
+      () => observer.complete(),
+    );
+  });
 
-// const filter = (func) => (stream$) =>
-//   new Observable((observer) => {
-//     stream$.subscribe(
-//       (x) => {
-//         func(x) && observer.next(x);
-//       },
-//       (error) => observer.error(error),
-//       () => observer.complete(),
-//     );
-//   });
+const filter = (func) => (stream$) =>
+  new Observable((observer) => {
+    stream$.subscribe(
+      (x) => {
+        func(x) && observer.next(x);
+      },
+      (error) => observer.error(error),
+      () => observer.complete(),
+    );
+  });
 
-// const tap = (func) => (stream$) => {
-//   stream$.subscribe((x) => {
-//     func(x);
-//   });
-//   return stream$;
-// };
+const tap = (func) => (stream$) => {
+  stream$.subscribe((x) => {
+    func(x);
+  });
+  return stream$;
+};
 
 const nums$ = new Observable((observer) => {
+  //observer is an object that inside includes next, error, complete
   [1, 2, 3, 4, 5].forEach((x) => {
     observer.next(x);
   });
@@ -51,14 +53,14 @@ const nums$ = new Observable((observer) => {
 });
 
 nums$
-  //    .pipe(
-  //      tap((x) => console.log("tap1", x)),
-  //      filter((x) => x % 2),
-  //      map((x) => x + 1),
-  //      tap((x) => console.log("tap2", x)),
-  //      map((x) => x * 2),
-  //      tap((x) => console.log("tap3", x)),
-  //    )
+  .pipe(
+    tap((x) => console.log("tap1", x)),
+    filter((x) => x % 2),
+    // map((x) => x + 1),
+    // tap((x) => console.log("tap2", x)),
+    // map((x) => x * 2),
+    // tap((x) => console.log("tap3", x)),
+  )
   .subscribe(
     (num) => {
       console.log(num);
